53078d5de90d8d2488866de29f5719b1977d1aa7
Gazi Hataş <gazihatas71@gmail.com>
2024-08-26 02:52:25 +0300

    Authentication process finish and Swagger docs  added!

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,20 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="JWT AUTH CORE v1",
+ *      description="Laravel Swagger API description",
+ *      @OA\Contact(
+ *          email="gazihatas71@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
 abstract class Controller
 {
     //
